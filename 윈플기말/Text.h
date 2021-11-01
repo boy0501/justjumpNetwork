7636a7c2bc313fa5944eb49800580874e469eefa
boy0501 <boy0501@naver.com>
2021-11-01 19:37:53 +0900

    한글입력기 Create
    작성자 : 이수민
    작업 내용
    - GetText함수 생성
    - wszComp,wsz1Comp변수 생성
    - isComposit (조합중인지 아닌지 판단변수 생성)
    - imm32.lib 추가
    - 기존 키보드 if-else 방식에서 switch방식으로 변환

--- 윈플기말/Text.h
+++ 윈플기말/Text.h
@@ -12,7 +12,10 @@ public:
 
 	
 	void UpdateFontSize(HWND& hwnd);
+	void pushByteChar(const char& c) { mText.pop_back(); mText += c; }
 	void pushChar(const char& c) { mText += c; }
+	void pushwChar(const wchar_t& c) { mText += c; }
+	void changewChar(const wchar_t& c) { mText[mText.size()-1] = c; }
 	void popChar() { mText.pop_back(); }
 	const int& getTextLen() const { return mText.length(); }
 	const SIZE& getFontLen() const { return mFontLength; }
