0426b7a3d9514d67b6c1d54238082a312016d00f
jpark142 <48274292+jpark142@users.noreply.github.com>
2021-11-28 18:10:22 +0900

    2021 11 28
    작업자: 박주용
    작업내용:
    - 이전 수정 작업에서 미처 발견하지 못한 로비 "5252~3p game" 텍스트 그려주는 방식 수정, setText함수 추가
    - 계획 일정 업데이트

--- Client/윈플기말/Text.h
+++ Client/윈플기말/Text.h
@@ -16,10 +16,12 @@ public:
 	void UpdateFontSize(HWND& hwnd);
 	void pushByteChar(const char& c) { mText.pop_back(); mText += c; }
 	void pushChar(const char& c) { mText += c; }
-	void pushString(std::wstring s) { mText += s + L"   ";}
+	void pushString(const std::wstring s) { mText += s + L"   ";}
 	void pushwChar(const wchar_t& c) { mText += c; }
 	void changewChar(const wchar_t& c) { if (mText.size() == 0) mText[0] = c; else mText[mText.size() - 1] = c; }	//한글입력할때를 위한 함수.
 	void popChar() { mText.pop_back(); }
+	void setText(const std::wstring s) { mText = s; }
+
 	const int& getTextLen() const { return mText.length(); }
 	const SIZE& getFontLen() const { return mFontLength; }
 	const std::string getNameTag() const { return mNameTag; }
